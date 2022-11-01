ac1fe99ca65507cb154fab912a1c9ec3f42393ec
Jafarscript <lihammedjafar@gmail.com>
2022-11-01 14:47:42 +0100

    Internship table design

--- src/components/Internship/Internship.jsx
+++ src/components/Internship/Internship.jsx
@@ -13,10 +13,65 @@ const Internship = ({toogleSelect}) => {
       </header>
       <div className="insight">
         <article>
+          <div>
           <h3>Internship Insights</h3>
           <p>In the eighteenth century the German philosopher Immanuel Kant developed a theory of knowledge in which knowledge about space can be both a priori and synthetic.</p>
+          </div>
         </article>
+        <section className='chart'>
+          <nav>
+            <div className='btns'>
+              <button>This week</button>
+              <button>This month</button>
+            </div>
+            <button className='btn-date'> <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1667303070/calendar-2_tifh1r.png" alt="" /> Select Date</button>
+          </nav>
+        </section>
       </div>
+      <table className='wrapper'>
+        <thead>
+          <tr>
+            <th>
+              <div className='rowh'>Internship Title <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1667303451/arrow-square-down_bmloz3.png" alt="" /></div>
+            </th>
+            <th>
+              <div className='rowh'>
+              Completion Period <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1667303451/arrow-square-down_bmloz3.png" alt="" />
+              </div>
+            </th>
+            <th>
+              <div className='rowh'>
+              Total Enrolled <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1667303451/arrow-square-down_bmloz3.png" alt="" />
+              </div>
+            </th>
+            <th>
+              <div className='rowh'>
+              Qualified Candidates <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1667303451/arrow-square-down_bmloz3.png" alt="" />
+              </div>
+            </th>
+            <th>Actions </th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>
+            <h4>Product Design GVI</h4>
+            <p>Amet minim mollit non deserunt est sit aliqua dolor do amet sint. </p>
+          </td>
+          <td>
+            <h4>120 days</h4>
+            <p>(created on 10/12/2021)</p>
+          </td>
+          <td>20,000</td>
+          <td> <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1667305309/Row_oojnkj.png" alt="" /></td>
+          <td>
+            <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1667305432/chart_xvjozy.png" alt="" />
+            <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1667305433/profile-2user_aaxk1r.png" alt="" />
+            <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1667305417/more-2_g4vx6s.png" alt="" />
+          </td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
